cb9800159e84cf0261380b0c6cc04572a078128c
Cyprian <cyprian.chailan@icloud.com>
2022-12-04 14:07:57 +0100

    update

--- src/main/java/com/ccyprian/encryptdecrypt/Controller.java
+++ src/main/java/com/ccyprian/encryptdecrypt/Controller.java
@@ -1,19 +1,13 @@
 package com.ccyprian.encryptdecrypt;
 
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.util.Base64;
 
 public class Controller {
